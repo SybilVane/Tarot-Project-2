bf5b688d122e687bedc38f45b0b0bc986158781a
Vanessa Zagone <vanessa.zgn@gmail.com>
2021-09-13 17:39:45 +0200

    Card model

--- models/Card.model.js
+++ models/Card.model.js
@@ -1,40 +1,43 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
-const cardSchema = new Schema({
-  
-  number: {
-    type: Number,
-    require: true
-  },
-  name: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  image: {
-    type: String,
-    required: true
-  },
-  uprightKeywords: {
-    type: String,
-    required: true
-  },
-  reverseKeywords: {
-    type: String,
-    required: true
-  },
-  uprightFullDescription: {
-    type: String,
-    required: true
+const cardSchema = new Schema(
+  {
+    number: {
+      type: Number,
+      require: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    uprightImage: {
+      type: String,
+    },
+    reverseImage: {
+      type: String,
+    },
+    uprightKeywords: {
+      type: String,
+      required: true,
+    },
+    reverseKeywords: {
+      type: String,
+      required: true,
+    },
+    uprightFullDescription: {
+      type: String,
+      required: true,
     },
     reverseFullDescription: {
       type: String,
-      required: true
-    }
-  },{  timestamps: true
-});
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-const Card = model("Card", cardSchema);
+const Card = model('Card', cardSchema);
 
 module.exports = Card;
