d77d8e2998353b37543d2c64ec2f93de4a1c2988
Vanessa Zagone <vanessa.zgn@gmail.com>
2021-09-15 18:43:22 +0200

    css links cards forum

--- models/User.model.js
+++ models/User.model.js
@@ -1,50 +1,53 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema({
-  
-  username: {
-    type: String,
-    unique: true,
-    require: true
-  },
-  password:{
-    type:String,
-    require: true
-  },
-  firstName:{
-    type:String,
-    require: true
-  },
-  lastName:{
-    type:String,
-    require: true
-  },
-  email:{
-    type:String,
-    require: true
-  },
-  
-  country: {
-    type: String,
-  },
-  
-  avatar: {
-    type: String,
-  },
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    firstName: {
+      type: String,
+      require: true,
+    },
+    lastName: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+    },
 
-  age: {
-    type: Number,
-    min: 16
-  },
-  role: {
-    type: String,
-    enum: ['GUEST', 'USER', 'ADMIN'],
-    default: 'GUEST'
-  },
-},{  timestamps: true
-});
+    country: {
+      type: String,
+    },
 
-const User = model("User", userSchema);
+    avatar: {
+      type: String,
+      default:
+        'https://res.cloudinary.com/dqwvgka1a/image/upload/v1631716200/uie5sdrtz0jzva0gwiuh.png',
+    },
+
+    age: {
+      type: Number,
+      min: 16,
+    },
+    role: {
+      type: String,
+      enum: ['GUEST', 'USER', 'ADMIN'],
+      default: 'GUEST',
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model('User', userSchema);
 
 module.exports = User;
