b90f35b583b9576d31564faabc340866b1a3eb30
Vanessa Zagone <vanessa.zgn@gmail.com>
2021-09-15 10:43:53 +0200

    cards database layout

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require("dotenv/config");
+require('dotenv/config');
 
 // ℹ️ Connects to the database
-require("./db");
+require('./db');
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
-const express = require("express");
+const express = require('express');
 
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
-const hbs = require("hbs");
+const hbs = require('hbs');
 
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
-require("./config")(app);
+require('./config')(app);
 
 // default value for title local
-const projectName = "tarot-project2";
-const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
+const projectName = 'tarot-project2';
+const capitalized = string =>
+  string[0].toUpperCase() + string.slice(1).toLowerCase();
 
-app.locals.siteTitle = `Tarot`;
+app.locals.siteTitle = `Tarot Sync`;
 
 // Session config
-require('./config/session.config')(app)
+require('./config/session.config')(app);
 
 // Routes
-require("./routes")(app)
+require('./routes')(app);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
+require('./error-handling')(app);
 
 module.exports = app;
